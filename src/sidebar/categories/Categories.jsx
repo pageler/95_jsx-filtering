b7db78e5eef74450a3124ba47022d16c252e5334
John Paul Pageler Jr <harakido1@yahoo.com>
2025-06-12 06:36:59 -0400

    Categories  component, pre-refactored code.

--- src/sidebar/categories/Categories.jsx
+++ src/sidebar/categories/Categories.jsx
@@ -1,9 +1,34 @@
 import "./Categories.css";
 
-import React from "react";
-
 function Categories() {
-  return <div>Categories</div>;
+  return (
+    <div>
+      <h2 className="sidebar-title">Categories</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default Categories;
