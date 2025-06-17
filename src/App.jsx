f198315fbb7a63237d61f1cb68686a03fae3234d
John Paul Pageler Jr <harakido1@yahoo.com>
2025-06-17 13:38:29 -0400

    Recommended file, pre-refactored code.

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
     <>
       <Nav />
       <Sidebar />
-      <Products />
       <Recommended />
+      <Products />
     </>
   );
 }
--- src/recommended/Recommended.jsx
+++ src/recommended/Recommended.jsx
@@ -1,7 +1,20 @@
 import "./Recommended.css";
 
 function Recommended() {
-  return <div>Recommended.jsx</div>;
+  return (
+    <>
+      <div>
+        <h2 className="recommended-title">Recommended</h2>
+        <div className="recommended-flex">
+          <button className="btns">All</button>
+          <button className="btns">Nike</button>
+          <button className="btns">Adidas</button>
+          <button className="btns">Puma</button>
+          <button className="btns">Vans</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Recommended;
