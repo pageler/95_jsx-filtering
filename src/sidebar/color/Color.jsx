5ad3604bff5d438159a1ca4488f3458e6637c673
John Paul Pageler Jr <harakido1@yahoo.com>
2025-06-17 18:50:26 -0400

    Color file, pre-refactored code.

--- src/sidebar/color/Color.jsx
+++ src/sidebar/color/Color.jsx
@@ -1,7 +1,40 @@
 import "./Color.css";
 
 function Color() {
-  return <div>Color.jsx</div>;
+  return (
+    <>
+      <div className="ml">
+        <h2 className="sidebar-title color-title">Color</h2>
+      </div>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Black
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Blue
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Red
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>Green
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className="checkmark"></span>White
+        </label>
+      </div>
+    </>
+  );
 }
 
 export default Color;
