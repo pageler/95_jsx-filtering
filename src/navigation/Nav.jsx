29ea159f870859e47d466f1f39834c00ab669c64
John Paul Pageler Jr <harakido1@yahoo.com>
2025-06-17 12:39:25 -0400

    Nav file.

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -1,7 +1,32 @@
 import "./Nav.css";
 
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import { FiHeart } from "react-icons/fi";
+
 function Nav() {
-  return <div>Nav.jsx</div>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your shoe search."
+        />
+      </div>
+
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
