60e961987f82d08e6e7a09fcc08296ca808f3972
John Paul Pageler Jr <harakido1@yahoo.com>
2025-06-17 16:58:13 -0400

    Category file, pre-refactored code.

--- src/sidebar/category/Category.jsx
+++ src/sidebar/category/Category.jsx
@@ -1,7 +1,36 @@
 import "./Category.css";
 
 function Category() {
-  return <div>Category.jsx</div>;
+  return (
+    <>
+      <div>
+        <h2 className="sidebar-title">Category</h2>
+      </div>
+
+      <div className="sidebar-items">
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
+    </>
+  );
 }
 
 export default Category;
