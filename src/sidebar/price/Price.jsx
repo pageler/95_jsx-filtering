fd2f984b01558911202a4c1946266735491686cc
John Paul Pageler Jr <harakido1@yahoo.com>
2025-06-18 19:16:46 -0400

    Price file refactored using Input component.

--- src/sidebar/price/Price.jsx
+++ src/sidebar/price/Price.jsx
@@ -2,14 +2,42 @@ import "./Price.css";
 
 import Input from "../../assets/components/Input";
 
-function Price() {
+function Price({ changeHandler }) {
   return (
     <>
       <div className="ml">
         <h2 className="sidebar-title price-title">Price</h2>
-      </div>
 
-      <Input />
+        <label className="sidebar-label-container">
+          <input type="radio" value="" name="test2" onChange={changeHandler} />
+          <span className="checkmark"></span> All
+        </label>
+
+        <Input
+          value={50}
+          title="$0 - $50"
+          name="test2"
+          onChange={changeHandler}
+        />
+        <Input
+          value={100}
+          title="$50 - $100"
+          name="test2"
+          onChange={changeHandler}
+        />
+        <Input
+          value={150}
+          title="$100 - $150"
+          name="test2"
+          onChange={changeHandler}
+        />
+        <Input
+          value={200}
+          title="Over $150"
+          name="test2"
+          onChange={changeHandler}
+        />
+      </div>
     </>
   );
 }
